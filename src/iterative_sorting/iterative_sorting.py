52d85ed29a5b3b94154535664679462849e8b19e
Shota Kiko <Shota.Kikozashvili@gmail.com>
2019-12-10 23:58:04 -0600

    added insertion sort/ commented logic

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -19,7 +19,7 @@ def selection_sort(list):
 
 
 
-list1 = [3, 3, 16, 87, 2]
+list1 = [3, 3, 16, 87, 2, 15, 2000, 1, 23, 13]
 
 print(selection_sort(list1))
 
@@ -29,15 +29,32 @@ def bubble_sort( arr ):
 
     for i in range(0, length):
 
-        for j in range(0, length - i - 1):
+        for j in range(0, length - i - 1): #to evaluate j and adjacent// subtracting i because as you proceed through loop last value will be sorted
             if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                arr[j], arr[j + 1] = arr[j + 1], arr[j] #swap the values in each iteration pair
 
     return arr
 print(bubble_sort(list1))
+
 
 
 # # STRETCH: implement the Count Sort function below
 # def count_sort( arr, maximum=-1 ):
 
 #     return arr
+
+
+
+#Extra Practice--- Insertion sort again
+
+def insertion_sort(li):
+    for i in range(1, len(li)): # start at 1 because li[0] will never enter the loop conditions
+        current_value = li[i]
+        j = i
+
+        while j > 0 and current_value < li[j - 1]:  #proceeds right to left till first index//
+            li[j] = li[j-1]  #shifts to the right
+            j -= 1
+        li[j] = current_value  #assigns the suspended value back into the array
+    return li
+print(insertion_sort(list1))
